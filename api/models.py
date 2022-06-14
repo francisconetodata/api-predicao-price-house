8b5fe67f24cd0015d3d56e6ddc0b89fb197c117f
Francisco Neto <francisconetodata@gmail.com>
2022-06-14 10:59:59 -0300

    contrucao do modelo ML

--- api/models.py
+++ api/models.py
@@ -1,3 +1,49 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class HouseBase(models.Model):
+    status = models.CharField(
+        'status',
+        max_length=20,
+        choices= (
+            ('for_sale','for_sale'),
+            ('ready_to_build','ready_to_build')
+        )
+    )
+    price = models.FloatField(
+        'price'
+    )
+    bed = models.IntegerField(
+        'bed'
+    )
+    bath = models.IntegerField(
+        'bath'
+    )
+    acre_lot = models.FloatField(
+        'acre_lot'
+    )
+    full_address = models.TextField(
+        'full_address'
+    )
+    street = models.TextField(
+        'street'
+    )
+    city = models.CharField(
+        'city',
+        max_length=50
+    )
+    state = models.CharField(
+        'state',
+        max_length=10
+    )
+    zip_code = models.FloatField(
+        'zip_code'
+    )
+    house_size = models.FloatField(
+        'house_size'
+    )
+    sold_date = models.DateField(
+        'sold_date'
+    )
