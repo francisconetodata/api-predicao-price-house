fc5d083e7c28be4a7da2d9ac856da1b643d2ccd8
Francisco Neto <francisconetodata@gmail.com>
2022-06-17 18:57:19 -0300

    Continuação - criação API

---
+++ api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from .models import HouseBase
+
+
+class HouseBaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HouseBase
+
+class PredizerPriceHouse(serializers.ModelSerializer):
+    class Meta:
+        model = HouseBase
+        fields = (
+            "status",
+            "bed",
+            "bath",
+            "acre_lot",
+            "city",
+            "state",
+            "zip_code",
+            "house_size",
+            "sold_date"
+        )
--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from requests import request
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
 
-# Create your views here.
+from rest_framework.schemas.openapi import AutoSchema
+from .models import HouseBase
+from .serializers import HouseBaseSerializer, PredizerPriceHouse
+
+
+
+class BaseAPIView(generics.ListCreateAPIView):
+    """
+
+    Fonte: https://www.kaggle.com/competitions/titanic
+    E-mail: francisconetodata@gmail.com
+
+
+    """
+    queryset = HouseBase.objects.all()
+    serializer_class = HouseBaseSerializer
